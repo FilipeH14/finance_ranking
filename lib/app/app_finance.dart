24d6f5b41978b7e6e5f3f279b04ed5f0587fe50c
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-03-23 14:59:05 -0300

    configurando adm de conexão

--- lib/app/app_finance.dart
+++ lib/app/app_finance.dart
@@ -1,16 +1,35 @@
+import 'package:finance_ranking/app/core/database/sqlite_adm_connection.dart';
 import 'package:finance_ranking/app/modules/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
-class AppFinance extends StatelessWidget {
+class AppFinance extends StatefulWidget {
+  const AppFinance({Key? key}) : super(key: key);
 
-  const AppFinance({ Key? key }) : super(key: key);
+  @override
+  State<AppFinance> createState() => _AppFinanceState();
+}
 
-   @override
-   Widget build(BuildContext context) {
-       return const MaterialApp(
-        title: 'Finance Rancking',
-        debugShowCheckedModeBanner: false,
-        home: SplashPage(),
-       );
+class _AppFinanceState extends State<AppFinance> {
+  final sqliteAdmConnection = SqliteAdmConnection();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(sqliteAdmConnection);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(sqliteAdmConnection);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Finance Rancking',
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    );
   }
 }
---
+++ lib/app/core/database/sqlite_adm_connection.dart
@@ -0,0 +1,22 @@
+import 'package:finance_ranking/app/core/database/sqlite_connection_factory.dart';
+import 'package:flutter/widgets.dart';
+
+class SqliteAdmConnection with WidgetsBindingObserver {
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+
+    final connection = SqliteConnectionFactory();
+
+    switch(state) {
+      case AppLifecycleState.resumed:
+        break;
+      case AppLifecycleState.inactive:
+      case AppLifecycleState.paused:
+      case AppLifecycleState.detached:
+        connection.closeConnection();
+        break;
+    }
+
+    super.didChangeAppLifecycleState(state);
+  }
+}
